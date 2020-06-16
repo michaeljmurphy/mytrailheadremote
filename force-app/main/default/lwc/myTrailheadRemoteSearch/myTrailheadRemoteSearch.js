c5a2077f0a5bab03871c50e3f91b549a538729e2
michael murphy <michaelmurphy@salesforce.com>
2020-06-16 09:06:34 +0200

    callout works

--- force-app/main/default/lwc/myTrailheadRemoteSearch/myTrailheadRemoteSearch.js
+++ force-app/main/default/lwc/myTrailheadRemoteSearch/myTrailheadRemoteSearch.js
@@ -1,3 +1,63 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire} from 'lwc';
+import getUsers from '@salesforce/apex/MyTrailheadRemoteController.getUsers';
+import getTrailmixes from '@salesforce/apex/MyTrailheadRemoteController.getTrailmixes';
 
-export default class MyTrailheadRemoteSearch extends LightningElement {}
+export default class MyTrailheadRemoteSearch extends LightningElement {
+    @api searchTarget;
+    userSearchString;
+    trailmixSearchString;
+    results;
+    error;
+
+    @wire(getUsers, { searchString: '$userSearchString' })
+    wiredUsers({ error, data }) {
+        if(error) {
+            this.error = 'Unknown error';
+            if(Array.isArray(error.body)) {
+                this.error = error.body.map(e => e.message).join(', ');
+            } else if (typeof error.body.message === 'string') {
+                this.error = error.body.message;
+            }
+            this.results = undefined;
+            console.log("error: " + this.error);
+        } else if (data) {
+            this.results = data;
+            console.log(data);
+        }
+    }
+
+    @wire(getTrailmixes, { searchString: '$trailmixSearchString' })
+    wiredTrailmixes({ error, data }) {
+        if(error) {
+            this.error = 'Unknown error';
+            if(Array.isArray(error.body)) {
+                this.error = error.body.map(e => e.message).join(', ');
+            } else if (typeof error.body.message === 'string') {
+                this.error = error.body.message;
+            }
+            this.results = undefined;
+            console.log("error: " + this.error);
+        } else if (data) {
+            this.results = data;
+            console.log(data);
+        }
+    }
+    
+    searchHandler(event) {
+        const isEnterKey = event.keyCode === 13;
+        console.log('searchHandler');
+        console.log('keyCode: ' + event.keyCode);
+        if (isEnterKey) {
+            console.log('isEnterKey');
+            console.log('searchTarget: ' + this.searchTarget);
+            
+            if(this.searchTarget === 'users') {
+                this.userSearchString = event.target.value;
+                console.log('userSearchString: ' + event.target.value);
+            } else if (this.searchTarget === 'trailmixes') {
+                this.trailmixSearchString = event.target.value;
+                console.log('trailmixSearchString: ' + event.target.value);
+            }
+        }
+    }    
+}
